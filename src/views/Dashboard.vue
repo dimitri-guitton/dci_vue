06f4c66efb6719e7b8442300f53b36e432063767
Dimitri <dimitri.guitton.dev@gmail.com>
2021-11-09 17:27:02 +0100

    Ajout de variables d'environnement

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -69,29 +69,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
-import TableWidget9 from "@/components/widgets/tables/Widget9.vue";
-import TableWidget5 from "@/components/widgets/tables/Widget5.vue";
-import ListWidget1 from "@/components/widgets/lists/Widget1.vue";
-import ListWidget2 from "@/components/widgets/lists/Widget2.vue";
-import ListWidget3 from "@/components/widgets/lists/Widget3.vue";
-import ListWidget5 from "@/components/widgets/lists/Widget5.vue";
-import ListWidget6 from "@/components/widgets/lists/Widget6.vue";
-import MixedWidget2 from "@/components/widgets/mixed/Widget2.vue";
-import MixedWidget5 from "@/components/widgets/mixed/Widget5.vue";
-import MixedWidget7 from "@/components/widgets/mixed/Widget7.vue";
-import MixedWidget10 from "@/components/widgets/mixed/Widget10.vue";
-import { setCurrentPageTitle } from "@/core/helpers/breadcrumb";
+import { defineComponent, onMounted } from 'vue';
+import TableWidget9 from '@/components/widgets/tables/Widget9.vue';
+import TableWidget5 from '@/components/widgets/tables/Widget5.vue';
+import ListWidget1 from '@/components/widgets/lists/Widget1.vue';
+import ListWidget2 from '@/components/widgets/lists/Widget2.vue';
+import ListWidget3 from '@/components/widgets/lists/Widget3.vue';
+import ListWidget5 from '@/components/widgets/lists/Widget5.vue';
+import ListWidget6 from '@/components/widgets/lists/Widget6.vue';
+import MixedWidget2 from '@/components/widgets/mixed/Widget2.vue';
+import MixedWidget5 from '@/components/widgets/mixed/Widget5.vue';
+import MixedWidget7 from '@/components/widgets/mixed/Widget7.vue';
+import MixedWidget10 from '@/components/widgets/mixed/Widget10.vue';
+import { setCurrentPageTitle } from '@/core/helpers/breadcrumb';
 
-export default defineComponent({
-  name: "dashboard",
-  components: {
-    TableWidget9,
-    TableWidget5,
-    ListWidget1,
-    ListWidget2,
-    ListWidget3,
-    ListWidget5,
+export default defineComponent( {
+                                  name:       'dashboard',
+                                  components: {
+                                    TableWidget9,
+                                    TableWidget5,
+                                    ListWidget1,
+                                    ListWidget2,
+                                    ListWidget3,
+                                    ListWidget5,
     ListWidget6,
     MixedWidget2,
     MixedWidget5,
@@ -100,7 +100,10 @@ export default defineComponent({
   },
   setup() {
     onMounted(() => {
-      setCurrentPageTitle("Dashboard");
+      setCurrentPageTitle( 'Dashboard' );
+      console.log( 'Version de l\'app : ', process.env.VUE_APP_VERSION );
+      console.log( 'Environnement : ', process.env.NODE_ENV );
+      console.log( 'URL API : ', process.env.VUE_APP_API_URL );
     });
   }
 });
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/metronic8/vue/demo1/" : "/"
+process.env.VUE_APP_VERSION = require( './package.json' ).version;
+module.exports              = {
+  // publicPath: process.env.NODE_ENV === "production" ? "/metronic8/vue/demo1/" : "/",
+  pluginOptions: {
+    electronBuilder: {
+      outputDir: 'releases',
+    },
+  },
 };
