9c860a2921301c652f6afd72c772a195d5f14dae
Dimitri <dimitri.guitton.dev@gmail.com>
2022-02-24 11:52:36 +0100

    build: Ajout de electron-icon-builder

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ process.env.VUE_APP_VERSION = require( './package.json' ).version;
 module.exports = {
     pluginOptions: {
         electronBuilder: {
-            outputDir:       'releases',
             nodeIntegration: true,
             externals:       [ 'clipboard' ],
         },
