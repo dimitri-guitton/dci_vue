85cc0ebf121a338f151fe3c05e8220f4eecdeb83
Dimitri <dimitri.guitton.dev@gmail.com>
2022-03-03 17:41:06 +0100

    feat(Algo): Nouvelle algo pour les Unité extérieur RO

--- src/services/file/RoAlgo2.ts
+++ src/services/file/RoAlgo2.ts
@@ -18,12 +18,37 @@ interface CoefTemperature {
     E: CoefTemperatureItem;
 }
 
-interface PacItem {
-    ref: string;
+/**
+ * Sortie en KW de la PAC
+ * -7 -> A -7°C
+ * -15 -> A -15°C
+ */
+interface PacOutput {
+    '-7': number;
+    '-15': number;
 }
 
+/**
+ * output selon la température de l'eau de la PAC
+ * 65 -> 65°C
+ * 55 -> 55°C
+ * 40 -> 40°C
+ */
+interface PacItem {
+    ref: string;
+    size: number;
+    output: {
+        65: PacOutput;
+        55: PacOutput;
+        40: PacOutput;
+    };
+}
+
+/**
+ * List des PAC par tension
+ */
 interface PacList {
-    monopahse: PacItem[];
+    monophase: PacItem[];
     triphase: PacItem[];
 }
 
@@ -141,7 +166,364 @@ export const calcRequiredPower = ( housing: PacHousing ): number => {
     return power / 1000;
 };
 
-export const getPacRo = () => {
-    console.log( 'IN GET PAC RO' );
+export const getUnitExtRo = ( requiredPower: number, housing: PacHousing ) => {
+    console.log( '%c GET PAC RO', 'background: #1EFF6C; color: #000000' );
+    const pacList: PacList = {
+        monophase: [
+            {
+                ref:    'EPRA08EV3', // 70°C
+                size:   8,
+                output: {
+                    65: {
+                        '-7':  8,
+                        '-15': 6.24,
+                    },
+                    55: {
+                        '-7':  7.55,
+                        '-15': 6.54,
+                    },
+                    40: {
+                        '-7':  7.51,
+                        '-15': 6.46,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA10EV3', // 70°C
+                size:   10,
+                output: {
+                    65: {
+                        '-7':  9.25,
+                        '-15': 6.73,
+                    },
+                    55: {
+                        '-7':  9.1,
+                        '-15': 7.57,
+                    },
+                    40: {
+                        '-7':  8.93,
+                        '-15': 7.47,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA12EV3', // 70°C
+                size:   12,
+                output: {
+                    65: {
+                        '-7':  10.75,
+                        '-15': 6.73,
+                    },
+                    55: {
+                        '-7':  10.58,
+                        '-15': 8.79,
+                    },
+                    40: {
+                        '-7':  10.37,
+                        '-15': 8.68,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA14EV3', // 70°C
+                size:   14,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    40: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA16EV3', // 70°C
+                size:   16,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    40: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA18EV3', // 70°C
+                size:   18,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    40: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                },
+            },
+            {
+                ref:    'ERGA04EV',
+                size:   4,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  4,
+                        '-15': 3.94,
+                    },
+                    40: {
+                        '-7':  5.34,
+                        '-15': 4.64,
+                    },
+                },
+            },
+            {
+                ref:    'ERGA06EV',
+                size:   6,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  4.91,
+                        '-15': 4.91,
+                    },
+                    40: {
+                        '-7':  6.25,
+                        '-15': 5.53,
+                    },
+                },
+            },
+            {
+                ref:    'ERGA08EV',
+                size:   8,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  6.74,
+                        '-15': 6.33,
+                    },
+                    40: {
+                        '-7':  7.29,
+                        '-15': 6.53,
+                    },
+                },
+            },
+            {
+                ref:    'EPGA11EV',
+                size:   11,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  10.12,
+                        '-15': 8.71,
+                    },
+                    40: {
+                        '-7':  10.86,
+                        '-15': 10.96,
+                    },
+                },
+            },
+            {
+                ref:    'EPGA14EV',
+                size:   14,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  11.38,
+                        '-15': 9.80,
+                    },
+                    40: {
+                        '-7':  12.22,
+                        '-15': 12.33,
+                    },
+                },
+            },
+            {
+                ref:    'EPGA16EV',
+                size:   16,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  12.65,
+                        '-15': 10.89,
+                    },
+                    40: {
+                        '-7':  13.57,
+                        '-15': 13.70,
+                    },
+                },
+            },
+        ],
+        triphase:  [
+            {
+                ref:    'EPRA08EW1', // 70°C
+                size:   8,
+                output: {
+                    65: {
+                        '-7':  8,
+                        '-15': 6.24,
+                    },
+                    55: {
+                        '-7':  7.55,
+                        '-15': 6.54,
+                    },
+                    40: {
+                        '-7':  7.51,
+                        '-15': 6.46,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA10EW1', // 70°C
+                size:   10,
+                output: {
+                    65: {
+                        '-7':  9.25,
+                        '-15': 6.73,
+                    },
+                    55: {
+                        '-7':  9.1,
+                        '-15': 7.57,
+                    },
+                    40: {
+                        '-7':  8.93,
+                        '-15': 7.47,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA12EW1', // 70°C
+                size:   12,
+                output: {
+                    65: {
+                        '-7':  10.75,
+                        '-15': 6.73,
+                    },
+                    55: {
+                        '-7':  10.58,
+                        '-15': 8.79,
+                    },
+                    40: {
+                        '-7':  10.37,
+                        '-15': 8.68,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA14EW1', // 70°C
+                size:   14,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    40: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA16EW1', // 70°C
+                size:   16,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    40: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                },
+            },
+            {
+                ref:    'EPRA18EW1', // 70°C
+                size:   18,
+                output: {
+                    65: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    55: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                    40: {
+                        '-7':  0,
+                        '-15': 0,
+                    },
+                },
+            },
+        ],
+    };
+
+    // Si on ne trouve pas dans la liste la tension souhaité on retourne null
+    if ( pacList[ housing.availableVoltage ] === undefined ) {
+        return null;
+    }
+
+    // On filtre la liste pour trouver la PAC souhaiter
+    const res = pacList[ housing.availableVoltage ].filter( ( pac: PacItem ) => {
+        // Si valeur de chauffage n'est pas défini
+        if ( pac.output[ heaterToValue( housing.heaters ) ] === undefined ) {
+            return false;
+        }
+
+        const baseTemp: number = getBaseTemperature( housing.climaticZone, housing.altitude );
+
+        let formatedBaseTemp;
+        if ( baseTemp < -7 ) {
+            formatedBaseTemp = '-7';
+        } else {
+            formatedBaseTemp = '-15';
+        }
+
+        // On retourne la PAC que si sont output est supérieur à la puissance requise
+        return pac.output[ heaterToValue( housing.heaters ) ][ formatedBaseTemp ] > requiredPower;
+    } );
+
+    console.log( 'RES', res );
 };
 
