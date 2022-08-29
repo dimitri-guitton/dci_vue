8bdc98325db033c2b0977f0ab956f0dea1fa4cfc
Dimitri <dimitri.guitton.dev@gmail.com>
2022-08-29 15:45:14 +0200

    feat(PDF): Mandat MaPrimeRenov - Case mandat administratif cochée par défaut

--- src/services/pdf/maPrimeRenovGeneratorV2.ts
+++ src/services/pdf/maPrimeRenovGeneratorV2.ts
@@ -60,9 +60,9 @@ export class MaPrimeRenovGeneratorV2 extends PdfGenerator {
         // On récup l'adresse
         const { address, zipCode, city } = getAddress( this._file );
 
-        let coord = { x: 108, y: 328 };
+        let coord = { x: 108, y: 326 };
         if ( this._file.beneficiary.civility === 'm' ) {
-            coord = { x: 66, y: 328 };
+            coord = { x: 66, y: 326 };
         }
 
         return {
@@ -75,42 +75,42 @@ export class MaPrimeRenovGeneratorV2 extends PdfGenerator {
                 },
                 {
                     text:             this._file.beneficiary.lastName.toUpperCase(),
-                    absolutePosition: { x: 80, y: 372 },
+                    absolutePosition: { x: 80, y: 370 },
                     characterSpacing: 5.6,
                 },
                 {
                     text:             this._file.beneficiary.firstName.toUpperCase(),
-                    absolutePosition: { x: 373, y: 372 },
+                    absolutePosition: { x: 373, y: 370 },
                     characterSpacing: 5.6,
                 },
                 {
                     text:             '.',
-                    absolutePosition: { x: 55, y: 362 },
+                    absolutePosition: { x: 55, y: 360 },
                     fontSize:         38,
                 },
                 {
                     text:             address.toUpperCase(),
-                    absolutePosition: { x: 58, y: 413 },
+                    absolutePosition: { x: 58, y: 411 },
                     characterSpacing: 5.6,
                 },
                 {
                     text:             zipCode,
-                    absolutePosition: { x: 102, y: 427 },
+                    absolutePosition: { x: 102, y: 425 },
                     characterSpacing: 5.6,
                 },
                 {
                     text:             city.toUpperCase(),
-                    absolutePosition: { x: 201, y: 428 },
+                    absolutePosition: { x: 201, y: 426 },
                     characterSpacing: 5.6,
                 },
                 {
                     text:             this._file.beneficiary.email.toUpperCase(),
-                    absolutePosition: { x: 109, y: 444 },
+                    absolutePosition: { x: 109, y: 442 },
                     characterSpacing: 5.6,
                 },
                 {
                     text:             this._file.beneficiary.mobile !== '' ? this._file.beneficiary.mobile : this._file.beneficiary.phone,
-                    absolutePosition: { x: 148, y: 460 },
+                    absolutePosition: { x: 148, y: 458 },
                     characterSpacing: 5.6,
                 },
             ],
