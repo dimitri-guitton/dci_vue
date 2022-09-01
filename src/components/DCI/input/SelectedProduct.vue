e11266a1cb9653e694e9c9a3215b0f69aa59e4b4
Dimitri <dimitri.guitton.dev@gmail.com>
2022-09-01 11:13:07 +0200

    feat(PAC RR): Suppression de l'alog de sélection des PAC

--- src/components/DCI/input/SelectedProduct.vue
+++ src/components/DCI/input/SelectedProduct.vue
@@ -1,88 +1,89 @@
 <template>
-  <template v-if="currentProduct !== undefined">
-    <div class="row" v-bind:class="{ 'mb-10': !alert, 'mb-2': alert }">
-      <div class="col-md-6 fv-row">
-        <Field :name="`selectedProducts[${index}].id`"
-               class="form-select"
-               as="select"
-               v-model="selectedId"
-               @change="onChangeProduct(selectedId)">
-          <option v-for="product in products" :key="product.id" :value="product.id">
-            {{ product.label }}
-          </option>
-        </Field>
-      </div>
-      <div class="col-md-2 fv-row">
-        <template v-if="refQuantityArea > 0">
-          <div class="input-group">
-            <Field
-                :disabled="true"
-                type="number"
-                class="form-control"
-                :name="`selectedProducts[${index}].quantity`"
-                placeholder="1"
-                v-model="refQuantityArea"
-            />
-            <div class="input-group-append">
-              <span class="input-group-text">U</span>
+    <template v-if="currentProduct !== undefined">
+        <div class="row" v-bind:class="{ 'mb-10': !alert, 'mb-2': alert }">
+            <div class="col-md-6 fv-row">
+                <Field :name="`selectedProducts[${index}].id`"
+                       class="form-select"
+                       as="select"
+                       v-model="selectedId"
+                       @change="onChangeProduct(selectedId)">
+                    <option v-for="product in products" :key="product.id" :value="product.id">
+                        <template v-if="showReference">{{ product.reference }} |</template>
+                        {{ product.label }}
+                    </option>
+                </Field>
             </div>
-          </div>
-        </template>
-        <template v-else>
-          <div class="input-group">
-            <Field
-                :disabled="true"
-                type="number"
-                class="form-control"
-                :name="`selectedProducts[${index}].quantity`"
-                placeholder="1"
-                v-model.number="currentProduct.quantity"
-            />
-            <div class="input-group-append">
-              <span class="input-group-text">U</span>
+            <div class="col-md-2 fv-row">
+                <template v-if="refQuantityArea > 0">
+                    <div class="input-group">
+                        <Field
+                            :disabled="true"
+                            type="number"
+                            class="form-control"
+                            :name="`selectedProducts[${index}].quantity`"
+                            placeholder="1"
+                            v-model="refQuantityArea"
+                        />
+                        <div class="input-group-append">
+                            <span class="input-group-text">U</span>
+                        </div>
+                    </div>
+                </template>
+                <template v-else>
+                    <div class="input-group">
+                        <Field
+                            :disabled="true"
+                            type="number"
+                            class="form-control"
+                            :name="`selectedProducts[${index}].quantity`"
+                            placeholder="1"
+                            v-model.number="currentProduct.quantity"
+                        />
+                        <div class="input-group-append">
+                            <span class="input-group-text">U</span>
+                        </div>
+                    </div>
+
+                </template>
             </div>
-          </div>
+            <div class="col-md-2 fv-row">
+                <Field
+                    v-model.number="currentProduct.pu"
+                    type="number"
+                    class="form-control"
+                    :name="`selectedProducts[${index}].pu`"
+                    placeholder="100"
+                    @change="onChangeProduct(selectedId)"
+                />
+                <ErrorMessage
+                    :name="`selectedProducts[${index}].pu`"
+                    class="fv-plugins-message-container invalid-feedback"
+                ></ErrorMessage>
+            </div>
+            <div class="col-md-2 fv-row d-flex justify-content-end align-items-center">
+                <template v-if="refQuantityArea > 0">
+                    <h5 class="mb-3">{{ numberToPrice( currentProduct.pu, quantityArea ) }}</h5>
+                </template>
+                <template v-else>
+                    <h5 class="mb-3">{{ numberToPrice( currentProduct.pu, currentProduct.quantity ) }}</h5>
+                </template>
 
-        </template>
-      </div>
-      <div class="col-md-2 fv-row">
-        <Field
-            v-model.number="currentProduct.pu"
-            type="number"
-            class="form-control"
-            :name="`selectedProducts[${index}].pu`"
-            placeholder="100"
-            @change="onChangeProduct(selectedId)"
-        />
-        <ErrorMessage
-            :name="`selectedProducts[${index}].pu`"
-            class="fv-plugins-message-container invalid-feedback"
-        ></ErrorMessage>
-      </div>
-      <div class="col-md-2 fv-row d-flex justify-content-end align-items-center">
-        <template v-if="refQuantityArea > 0">
-          <h5 class="mb-3">{{ numberToPrice( currentProduct.pu, quantityArea ) }}</h5>
-        </template>
-        <template v-else>
-          <h5 class="mb-3">{{ numberToPrice( currentProduct.pu, currentProduct.quantity ) }}</h5>
-        </template>
-
-      </div>
-    </div>
-    <div class="row mb-10" v-if="alert">
-      <div class="col-md-8">
-        <div class="alert alert-primary d-flex align-items-center p-5 mt-5">
-          <i class="fa fa-exclamation fs-2hx text-primary me-5"></i>
-          <div class="d-flex flex-column" v-html="htmlAlert"></div>
+            </div>
         </div>
-      </div>
-    </div>
-  </template>
-  <template v-else>
-    <div class="row mb-10">
-      <p>Aucun produit trouvé</p>
-    </div>
-  </template>
+        <div class="row mb-10" v-if="alert">
+            <div class="col-md-8">
+                <div class="alert alert-primary d-flex align-items-center p-5 mt-5">
+                    <i class="fa fa-exclamation fs-2hx text-primary me-5"></i>
+                    <div class="d-flex flex-column" v-html="htmlAlert"></div>
+                </div>
+            </div>
+        </div>
+    </template>
+    <template v-else>
+        <div class="row mb-10">
+            <p>Aucun produit trouvé</p>
+        </div>
+    </template>
 
 </template>
 <script lang="ts">
@@ -92,89 +93,97 @@ import { ErrorMessage, Field } from 'vee-validate';
 import { numberToPrice } from '@/services/commonService';
 
 export default defineComponent( {
-                                  name:       'selected-product',
-                                  components: {
-                                    Field,
-                                    ErrorMessage,
-                                  },
-                                  props:      {
-                                    index:            {
-                                      type:    Number,
-                                      default: 0,
+                                    name:       'selected-product',
+                                    components: {
+                                        Field,
+                                        ErrorMessage,
                                     },
-                                    quantityArea:     {// Qauntité au metre carré
-                                      type:    Number,
-                                      default: 0,
+                                    props:      {
+                                        index:            {
+                                            type:    Number,
+                                            default: 0,
+                                        },
+                                        quantityArea:     { // Quantité au metre carré
+                                            type:    Number,
+                                            default: 0,
+                                        },
+                                        selectedProducts: {
+                                            type:    Array as () => Product[],
+                                            default: () => [],
+                                        },
+                                        products:         {
+                                            type:    Array as () => Product[],
+                                            default: () => [],
+                                        },
+                                        showReference:    {
+                                            type:    Boolean,
+                                            default: false,
+                                        },
+                                        // Affichage du alert en dessous du produit
+                                        alert: String,
                                     },
-                                    selectedProducts: {
-                                      type:    Array as () => Product[],
-                                      default: () => [],
+                                    emits:      [ 'selectedProductIsUpdated' ],
+                                    setup( props, ctx ) {
+                                        let currentProduct = ref<Product>();
+                                        const selectedId   = ref<number>();
+
+                                        const onChangeProduct = ( value ) => {
+                                            currentProduct.value = props.products.find( p => p.id === value );
+                                            ctx.emit( 'selectedProductIsUpdated', currentProduct.value, 'product' );
+                                        };
+
+                                        console.log( 'products IN SelectedProduct.vue -->', props.products );
+                                        console.log( 'INDEX -->', props.index );
+                                        console.log( props.selectedProducts[ props.index ] );
+                                        if ( props.selectedProducts.length > 0 && props.selectedProducts[ props.index ] !== undefined ) {
+                                            console.log( 'IF' );
+                                            currentProduct = ref( props.selectedProducts[ props.index ] );
+                                        } else {
+                                            console.log( 'ELSE' );
+                                            currentProduct = ref( props.products[ 0 ] );
+                                            onChangeProduct( props.products[ 0 ].id );
+                                        }
+
+
+                                        const refQuantityArea = toRef( props, 'quantityArea' );
+
+                                        // Récupère le texte de l'alerte check si il y a des données envoyé et les remplaces
+                                        const htmlAlert = computed( () => {
+                                            if ( props.alert === undefined ) {
+                                                return '';
+                                            }
+                                            // keep a map of all your variables
+                                            const valueMap = {
+                                                size: currentProduct.value?.size,
+                                            };
+
+                                            let value     = props.alert;
+                                            const allKeys = Object.keys( valueMap );
+                                            allKeys.forEach( ( key ) => {
+                                                const myRegExp = new RegExp( `{{ ${key} }}`, 'i' );
+                                                value          = value.replace( myRegExp, valueMap[ key ] );
+                                            } );
+                                            return value;
+                                        } );
+
+
+                                        const resetSelectedValue = ( products: Product[] ) => {
+                                            currentProduct.value = products[ 0 ];
+                                            selectedId.value     = currentProduct.value?.id;
+                                            return currentProduct.value;
+                                        };
+
+                                        selectedId.value = currentProduct.value?.id;
+
+                                        return {
+                                            onChangeProduct,
+                                            resetSelectedValue,
+                                            refQuantityArea,
+                                            htmlAlert,
+                                            selectedId,
+                                            currentProduct,
+                                            numberToPrice,
+                                        };
                                     },
-                                    products:         {
-                                      type:    Array as () => Product[],
-                                      default: () => [],
-                                    },
-                                    // Affichage du alert en dessous du produit
-                                    alert: String,
-                                  },
-                                  emits:      [ 'selectedProductIsUpdated' ],
-                                  setup( props, ctx ) {
-                                    let currentProduct = ref<Product>();
-                                    const selectedId   = ref<number>();
-
-                                    const onChangeProduct = ( value ) => {
-                                      currentProduct.value = props.products.find( p => p.id === value );
-                                      ctx.emit( 'selectedProductIsUpdated', currentProduct.value, 'product' );
-                                    };
-
-                                    console.log( 'PROPS', props.products );
-                                    if ( props.selectedProducts.length > 0 ) {
-                                      currentProduct = ref( props.selectedProducts[ 0 ] );
-                                    } else {
-                                      currentProduct = ref( props.products[ 0 ] );
-                                      onChangeProduct( props.products[ 0 ].id );
-                                    }
-
-
-                                    const refQuantityArea = toRef( props, 'quantityArea' );
-
-                                    // Récupère le texte de l'alerte check si il y a des données envoyé et les remplaces
-                                    const htmlAlert = computed( () => {
-                                      if ( props.alert === undefined ) {
-                                        return '';
-                                      }
-                                      // keep a map of all your variables
-                                      const valueMap = {
-                                        size: currentProduct.value?.size,
-                                      };
-
-                                      let value     = props.alert;
-                                      const allKeys = Object.keys( valueMap );
-                                      allKeys.forEach( ( key ) => {
-                                        const myRegExp = new RegExp( `{{ ${key} }}`, 'i' );
-                                        value          = value.replace( myRegExp, valueMap[ key ] );
-                                      } );
-                                      return value;
-                                    } );
-
-
-                                    const resetSelectedValue = ( products: Product[] ) => {
-                                      currentProduct.value = products[ 0 ];
-                                      selectedId.value     = currentProduct.value?.id;
-                                      return currentProduct.value;
-                                    };
-
-                                    selectedId.value = currentProduct.value?.id;
-
-                                    return {
-                                      onChangeProduct,
-                                      resetSelectedValue,
-                                      refQuantityArea,
-                                      htmlAlert,
-                                      selectedId,
-                                      currentProduct,
-                                      numberToPrice,
-                                    };
-                                  },
                                 } );
 </script>
