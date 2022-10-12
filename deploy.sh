604788c196434d95d5c65395f49536986b54017d
Dimitri <dimitri.guitton.dev@gmail.com>
2022-10-12 17:36:12 +0200

    ci: Script de deploy

--- deploy.sh
+++ deploy.sh
@@ -3,65 +3,57 @@
 # MES FONCTIONS
 
 # Met à jour la version selon le type de nouvelle version
-updateVersion()
-{
-error="false"
-case $1 in
- 1)
+updateVersion() {
+  error="false"
+  case $1 in
+  1)
     npm version major
     ;;
- 2)
+  2)
     npm version minor
     ;;
- 3)
+  3)
     npm version patch
     ;;
- *)
+  *)
     error="true"
-   ;;
-esac
+    ;;
+  esac
 
-echo "$error"
+  echo "$error"
 }
 
 # Lancement du script
-start()
-{
+start() {
 
-python3 update_package.py
+  echo "Changement de version : "
 
-echo "Changement de version : "
+  select item in "- Majeur- " "- Mineur -" "- Patch -"; do
+    error=$(updateVersion $REPLY)
 
-select item in "- Majeur- " "- Mineur -" "- Patch -"
-do
-  error=$( updateVersion $REPLY )
+    if [ "$error" == "true" ]; then
+      echo "Error: Please try again (select 1..3)!"
+    else
+      PACKAGE_VERSION=$(cat package.json |
+        grep version |
+        head -1 |
+        awk -F: '{ print $2 }' |
+        sed 's/[",]//g')
 
-  if [ "$error" == "true" ]
-  then
-    echo "Error: Please try again (select 1..3)!"
-  else
-    PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-
-    echo "Nouvelle version" $PACKAGE_VERSION
-#    git push && git push --tags
-    break;
-  fi
-done
+      echo "Nouvelle version" $PACKAGE_VERSION
+      git push && git push --tags
+      break
+    fi
+  done
 }
 
-OLD_PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
+OLD_PACKAGE_VERSION=$(cat package.json |
+  grep version |
+  head -1 |
+  awk -F: '{ print $2 }' |
+  sed 's/[",]//g')
 
 echo "Version précédente : $OLD_PACKAGE_VERSION"
 
-python3 store_old_version.py $OLD_PACKAGE_VERSION
-
-
-start
+echo "CHECK LE PACKAGE JSON"
+#start
