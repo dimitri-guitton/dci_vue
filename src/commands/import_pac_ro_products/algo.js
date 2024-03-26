1314ad368e931fc1435766f2dcfd7c76455ee86d
Dimitri <dimitri.guitton.dev@gmail.com>
2024-03-26 10:34:09 +0100

    feat(PAC): ajout de nouveaux produits

--- src/commands/import_pac_ro_products/algo.js
+++ src/commands/import_pac_ro_products/algo.js
@@ -1,13 +1,16 @@
 // Fait une requete api sur https://erp.local/admin/product/json-ro pour récupérer un JSON
 // Ecrit le résultat dans un fichier selon le type de données
 
+
+// POUR LANCER LA COMMANDE EN DEV :  NODE_TLS_REJECT_UNAUTHORIZED='0' node src/commands/import_pac_ro_products/algo.js
+
 const axios = require( 'axios' );
 const fs    = require( 'fs' );
 
 async function getData() {
     // Requete http
-    // const response = await axios.get( 'https://127.0.0.1:8000/data/pac-ro?pass=9AFDC2F8C9BC394E9872B1839FFA4' );
-    const response = await axios.get( 'https://admin.eco-atl.fr/data/pac-ro?pass=9AFDC2F8C9BC394E9872B1839FFA4' );
+    const response = await axios.get( 'https://127.0.0.1:8000/data/pac-ro?pass=9AFDC2F8C9BC394E9872B1839FFA4' );
+    // const response = await axios.get( 'https://admin.eco-atl.fr/data/pac-ro?pass=9AFDC2F8C9BC394E9872B1839FFA4' );
     
     if ( response.status !== 200 ) {
         throw new Error( 'Error while fetching data' );
